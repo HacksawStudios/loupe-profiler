1efeaf46436a1b1a7ba11c2d6c01e64e32b68c98
eric <eric@eric.jonsson-hederstedt-debian>
2025-07-30 13:35:17 +0200

    test: Add unit tests for the profiler

---
+++ test/TestMain.hx
@@ -0,0 +1,89 @@
+package;
+
+import buddy.*;
+import hacksaw.profiler.Profiler;
+import haxe.Timer;
+
+using buddy.Should;
+
+@:build(hacksaw.profiler.Profiler.injectProfiler())
+class Foo {
+	@:profile
+	public function bar() {
+		trace("bar");
+	}
+
+	public function new() {}
+}
+
+// Implement "Buddy" and define an array of classes within the brackets:
+class TestMain extends buddy.SingleSuite {
+	var object:Dynamic = null;
+
+	public function new() {
+		// A test suite:
+		describe("Testing", {
+			Profiler.profileBlockStart("block1");
+			Sys.sleep(0.1);
+
+			Profiler.profileBlockStart("block2");
+			Sys.sleep(0.1);
+
+			Profiler.profileBlock("block3", {
+				Sys.sleep(0.1);
+			});
+
+			Profiler.profileBlockEnd();
+
+			var foo:Foo = new Foo();
+			foo.bar(); // Profiled through macro
+
+			Sys.sleep(0.1);
+
+			Profiler.profileBlockStart("block2");
+			Sys.sleep(0.1);
+			Profiler.profileBlockEnd();
+			Profiler.profileBlockEnd();
+
+			object = Profiler.dumpToObject();
+			trace(object);
+
+			it("should have the displayTimeUnit ms", {
+				object.displayTimeUnit.should.be("ms");
+			});
+			it("should have an even amount of traceEvents", {
+				final isTraceEventsEven = (object.traceEvents.length % 2) == 0;
+				isTraceEventsEven.should.be(true); // Number of begin and end traces must match!
+			});
+			it("should have 10 total trace events for this test", {
+				object.traceEvents.length.should.be(10); // This example should only produce 10 traces.
+			});
+			it("should register all traces for this test", {
+				final hasBlock1Trace = hasTraceEvent("block1");
+				final hasBlock2Trace = hasTraceEvent("block2");
+				final hasBlock3Trace = hasTraceEvent("block3");
+				final hasFooBarTrace = hasTraceEvent("Foo:bar");
+				final hasBlock2_2Trace = hasTraceEvent("block2", 1);
+				hasBlock1Trace.should.be(true);
+				hasBlock2Trace.should.be(true);
+				hasBlock3Trace.should.be(true);
+				hasFooBarTrace.should.be(true);
+				hasBlock2_2Trace.should.be(true);
+			});
+		});
+	}
+
+	function hasTraceEvent(name:String, skip:Int = 0, ph:String = "B"):Bool {
+		for (i in 0...object.traceEvents.length) {
+			final traceEvent = object.traceEvents[i];
+			if (traceEvent.name == name && traceEvent.ph == ph) {
+				if (skip > 0) {
+					skip--;
+					continue;
+				}
+				return true;
+			}
+		}
+		return false;
+	}
+}
